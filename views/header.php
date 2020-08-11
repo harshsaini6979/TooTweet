12d944e8c8934c12c80e61750f08a82eba4eb5de
Harsh Saini <68505151+harshsaini6979@users.noreply.github.com>
2020-08-11 19:12:46 +0530

    Update header.php

--- views/header.php
+++ views/header.php
@@ -1,12 +1,12 @@
 <html>
     <head>
-        <title>Twitter</title>
+        <title>TooTweet</title>
         <link rel="stylesheet" type="text/css" href="http://harshsainiprojects-com.stackstaging.com/twitter/styles.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <head>
     <body class="d-flex flex-column h-100">
         <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-        <a class="navbar-brand btn outline-dark" href="http://harshsainiprojects-com.stackstaging.com/twitter/index.php">TWITTER</a>
+        <a class="navbar-brand btn outline-dark" href="http://harshsainiprojects-com.stackstaging.com/twitter/index.php">TooTweet</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
